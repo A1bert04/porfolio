c938aecd14be25a23a83efa2ef9d0039509b2475
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2023-01-26 23:13:43 +0100

    Tailwind config route bug fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         "./index.html",
         "./src/*.{js,ts,jsx,tsx}",
         "./src/components/*.{js,ts,jsx,tsx}",
-        "./src/sections/*.{js,ts,jsx,tsx}",
+        "./src/pages/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
